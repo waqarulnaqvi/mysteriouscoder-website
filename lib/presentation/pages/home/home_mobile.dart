7c775a9c79cd6c243783d90c55eb91853dd1c329
Nirvana <syedwaqarul786@gmail.com>
2025-02-05 00:04:00 +0530

    Update 04-02-2025

--- lib/presentation/pages/home/home_mobile.dart
+++ lib/presentation/pages/home/home_mobile.dart
@@ -1,38 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:mysteriouscoder/presentation/widgets/drawer/custom_drawer.dart';
+import 'package:mysteriouscoder/shared/constants.dart';
 import '../../../shared/global.dart';
 import '../../../shared/styles.dart';
 
+class HomeMobile extends StatefulWidget {
+  HomeMobile({super.key});
 
-class HomeMobile extends StatelessWidget {
-   HomeMobile({super.key});
+  @override
+  State<HomeMobile> createState() => _HomeMobileState();
+}
+
+class _HomeMobileState extends State<HomeMobile> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  bool isDark = false;
 
   @override
   Widget build(BuildContext context) {
-    double w=MediaQuery.of(context).size.width;
-    double h=MediaQuery.of(context).size.height;
+    double w = MediaQuery.of(context).size.width;
+    double h = MediaQuery.of(context).size.height;
     return Scaffold(
       key: _scaffoldKey,
       backgroundColor: bgColor,
-      endDrawer: CustomDrawer(w: w*0.75,h: h,),
+      endDrawer: CustomDrawer(
+        w: w * 0.75,
+        h: h,
+      ),
       endDrawerEnableOpenDragGesture: false,
       appBar: _appBar(),
-
-    body: SingleChildScrollView(
-      child: Column(
-        children:<Widget> [
-
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[],
+        ),
       ),
-    ),
     );
   }
 
-
-
- AppBar _appBar()
-  {
+  AppBar _appBar() {
     return AppBar(
       leading: const Padding(
         padding: EdgeInsets.all(6.0),
@@ -41,15 +45,26 @@ class HomeMobile extends StatelessWidget {
           radius: 20,
         ),
       ),
-      foregroundColor: Colors.white,
-      backgroundColor: dark,
-      title: Text('Mysterious Coder',style: reusableTextStyle()),
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      title: Text(Constants.title,style: reusableTextStyle(color:  isDark? Colors.white : Colors.black)),
       actions: [
+        IconButton(
+            onPressed: () {
+              isDark = !isDark;
+              setState(() {});
+            },
+            icon: Image(image: AssetImage(isDark? "assets/icons/light_theme.png" :"assets/icons/dark_icon.png"),
+              width: 30,
+              height: 30,
+              color: isDark? Colors.white : Colors.black,
+            )
+        ),
+        const SizedBox(width: 5),
         InkWell(
             onTap: (){
               _scaffoldKey.currentState!.openEndDrawer();
             },
-            child: const Image(image: AssetImage("assets/icons/menu.png"),color: Colors.white,width: 30,height: 30,)),
+            child: Image(image: const AssetImage("assets/icons/menu.png"),width: 30,height: 30,color: isDark? Colors.white : Colors.black,)),
       ],
     );
   }
---
+++ lib/shared/constants.dart
@@ -0,0 +1,18 @@
+
+class Constants{
+  // app constants
+  static const String title = 'Mysterious Coder';
+  static const String appVersion = '1.0.0';
+  static const String appDescription = 'Tazeen Naqvi Portfolio';
+
+  // common UI constants
+  static const double borderRadius = 10.0;
+  static const double minButtonHeight = 48.0;
+  static const double buttonTextSize = 15.0;
+
+  // firebase constants
+  static const String collectionUser = 'users';
+
+  // shared preferences constants
+  static const String onboardingCompleted = 'onboarding_completed';
+}
