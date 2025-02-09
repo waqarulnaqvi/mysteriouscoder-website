bb2b5502a3d7fa68323a06885f24931d82521730
Nirvana <syedwaqarul786@gmail.com>
2025-02-10 01:22:21 +0530

    UPDATE 10-02-2025

--- lib/presentation/widgets/layout/common/main_part.dart
+++ lib/presentation/widgets/layout/common/main_part.dart
@@ -20,31 +20,102 @@ class MainPart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       width: w,
-      height: h*0.7,
-      // height: w < Constants.maxPhoneWidth
-      //     ? h * 1.6
-      //     : w < Constants.maxTabletWidth
-      //     ? h * 1.53
-      //     : h * 1.2,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
+      // height: h*0.7,
+      height: w < Constants.maxPhoneWidth
+          ? h
+          : w < Constants.maxTabletWidth
+          ? h * 0.9
+          : h * 0.7,
+      child:  ResponsiveLayout(mobile: mobile(context), tablet: mobile(context), desktop: web(context)),
+    );
+  }
+
+  Widget mobile(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              spacerH(30),
-              Row(
+
+          const EntranceFader(
+            offset: Offset(0, 0),
+            delay: Duration(seconds: 1),
+            duration: Duration(milliseconds: 800),
+            child: ZoomAnimations(),
+          ),
+
+          spacerH(10),
+
+          Text(
+            "Mysterious Coder",
+            style: reusableTextStyle(
+                fontSize: 30, color: Theme.of(context).colorScheme.onSurface),
+          ),
+          FittedBox(
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text("Hi there, Welcome to Official Mysterious Coder Page",
+                    style: TextStyle(
+                        color: Theme.of(context).colorScheme.onSurface,
+                        fontSize: 18),
+                textAlign: TextAlign.center,),
+                spacerW(10),
+                Image.asset(
+                  StaticImage.hi,
+                  height: 35,
+                ),
+              ],
+            ),
+          ),
+          spacerH(),
+          Text(
+              "I'm a Senior Digital Marketing Specialist with over 5 years of experience in the field. I have worked with various clients and helped them achieve their marketing goals. I have a strong understanding of the latest marketing trends and techniques. I am passionate about marketing and always looking for new ways to improve my skills.",
+              style: TextStyle(
+                  color: Theme.of(context).colorScheme.onSurface, fontSize: 20),
+          textAlign: TextAlign.center,),
+          spacerH(),
+          ColorChangeButton(
+            text: "Get in Touch", onTap: () {  },
+
+          ),
+
+          // ResponsiveLayout(mobile: mobile(context), tablet: mobile(context), desktop: web(context)),
+        ],
+      ),
+    );
+  }
+
+  Widget web(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            spacerH(30),
+            Text(
+              "Mysterious Coder",
+              style: reusableTextStyle(
+                  fontSize: 30, color: Theme.of(context).colorScheme.onSurface),
+            ),
+            spacerH(5),
+            Flexible(
+              child: Row(
                 mainAxisSize: MainAxisSize.min,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Text("Hi there, Welcome to My Space ",
-                      style: TextStyle(
-                          color: Theme.of(context).colorScheme.onSurface,
-                          fontSize: 18)),
+                  Text("Hi there, Welcome to Official Mysterious Coder Page",
+                    style: TextStyle(
+                        color: Theme.of(context).colorScheme.onSurface,
+                        fontSize: 18),
+                    textAlign: TextAlign.center,),
                   spacerW(10),
                   Image.asset(
                     StaticImage.hi,
@@ -52,152 +123,34 @@ class MainPart extends StatelessWidget {
                   ),
                 ],
               ),
-              spacerH(10),
-              Text(
-                "Mysterious Coder",
-                style: reusableTextStyle(
-                    fontSize: 30, color: Theme.of(context).colorScheme.onSurface),
-              ),
-              spacerH(),
+            ),
+            spacerH(),
+            SizedBox(
+              width: w*0.5,
+              child: Text(
+                  "I'm a Senior Digital Marketing Specialist with over 5 years of experience in the field. I have worked with various clients and helped them achieve their marketing goals. I have a strong understanding of the latest marketing trends and techniques. I am passionate about marketing and always looking for new ways to improve my skills.",
+                style: TextStyle(
+                    color: Theme.of(context).colorScheme.onSurface, fontSize: 20),
+            ),
+            ),
+            spacerH(),
+            ColorChangeButton(
+              text: "Get in Touch", onTap: () {  },
 
-              ColorChangeButton(
-                text: "Get in Touch", onTap: () {  },
+            ),
 
-              ),
-
-              // ResponsiveLayout(mobile: mobile(context), tablet: mobile(context), desktop: web(context)),
-            ],
-          ),
-          const EntranceFader(
+            // ResponsiveLayout(mobile: mobile(context), tablet: mobile(context), desktop: web(context)),
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 30),
+          child: const EntranceFader(
             offset: Offset(0, 0),
             delay: Duration(seconds: 1),
             duration: Duration(milliseconds: 800),
             child: ZoomAnimations(),
           ),
-        ],
-      ),
-    );
-  }
-
-  Widget mobile(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15),
-          child: Text(
-              "I'm a Senior Digital Marketing Specialist with over 5 years of experience in the field. I have worked with various clients and helped them achieve their marketing goals. I have a strong understanding of the latest marketing trends and techniques. I am passionate about marketing and always looking for new ways to improve my skills.",
-              style: reusableTextStyle(fontSize: 20, color: Colors.white)),
         ),
-        spacerH(30),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15),
-          child: Container(
-            width: 180,
-            height: 50,
-            padding: const EdgeInsets.all(10),
-            decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.onPrimary,
-                borderRadius: BorderRadius.circular(10),
-                border: Border.all(
-                  color: Colors.white,
-                  width: 2,
-                )),
-            child: Material(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10)),
-              color: Colors.transparent,
-              child: InkWell(
-                child: Center(
-                  child: Text(
-                    "HOW I CAN HELP",
-                    style: reusableTextStyle(fontSize: 15),
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ),
-        spacerH(),
-        Center(
-          child: ConstrainedBox(
-              constraints: const BoxConstraints(
-                maxWidth: Constants.maxPhoneWidth / 1.4,
-              ),
-              child: Image(
-                image: const AssetImage("assets/images/help_image.png"),
-                width: w,
-                fit: BoxFit.cover,
-              )),
-        )
-      ],
-    );
-  }
-
-  Widget web(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Expanded(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 15),
-                  child: Text(
-                      "I'm a Senior Digital Marketing Specialist with over 5 years of experience in the field. I have worked with various clients and helped them achieve their marketing goals. I have a strong understanding of the latest marketing trends and techniques. I am passionate about marketing and always looking for new ways to improve my skills.",
-                      style:
-                      reusableTextStyle(fontSize: 20, color: Colors.white)),
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 15),
-                  child: Container(
-                    width: 180,
-                    height: 50,
-                    padding: const EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                        color: Theme.of(context).colorScheme.onPrimary,
-                        borderRadius: BorderRadius.circular(10),
-                        border: Border.all(
-                          color: Colors.white,
-                          width: 2,
-                        )),
-                    child: Material(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10)),
-                      color: Colors.transparent,
-                      child: InkWell(
-                        child: Center(
-                          child: Text(
-                            "HOW I CAN HELP",
-                            style: reusableTextStyle(fontSize: 15),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ]),
-        ),
-        Expanded(
-          child: Center(
-            child: ConstrainedBox(
-                constraints: const BoxConstraints(
-                  maxWidth: Constants.maxPhoneWidth / 1.4,
-                ),
-                child: Image(
-                  image: const AssetImage("assets/images/help_image.png"),
-                  width: w,
-                  fit: BoxFit.cover,
-                )),
-          ),
-        )
       ],
     );
   }
