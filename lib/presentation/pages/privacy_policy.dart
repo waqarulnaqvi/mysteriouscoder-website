ae3a81bbe8a57cb8a51adff1b44abd3c22a4e232
Nirvana <syedwaqarul786@gmail.com>
2025-02-23 17:14:35 +0530

    UPDATE 23-02-2025

--- lib/presentation/pages/privacy_policy.dart
+++ lib/presentation/pages/privacy_policy.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:mysteriouscoder/presentation/providers/theme_provider.dart';
 import 'package:mysteriouscoder/presentation/widgets/privacy_policy_description.dart';
 import 'package:provider/provider.dart';
